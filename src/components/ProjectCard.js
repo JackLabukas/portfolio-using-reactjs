6e1b5b075e29bcce6ce23e5e9d72b751d1c86f0b
JackLabukas <labukasgediminas@gmail.com>
2023-02-03 19:45:43 +0000

    added some components, other basic files

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ProjectCard = ({ id, image, title }) => {
+  return (
+    <Link to={`/project/${id}`}>
+      <div>
+        <img className="project-image" src={image} alt={title} />
+        <h1>{title}</h1>
+      </div>
+    </Link>
+  );
+};
